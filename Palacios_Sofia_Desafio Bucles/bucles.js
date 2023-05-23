8b381a07bc3cd3ae3705184313ccf2fba9124459
Sofia-Palacios-Orrego <133055759+Sofia-Palacios-Orrego@users.noreply.github.com>
2023-05-23 12:42:44 -0400

    Add files via upload

---
+++ Palacios_Sofia_Desafio Bucles/bucles.js
@@ -0,0 +1,35 @@
+for(var i= 1; i<=10; i++){
+    if(i % 2 == 1){
+        console.log(i);
+    }
+}
+
+
+
+for (var x=100; x>=0; x--){
+    if(x % 3 ==0){
+        console.log(x);
+    }
+}
+
+
+
+for (var t=4; t>=-4; t=t-1.5){
+    console.log(t);
+}
+
+
+
+var sum = 0;
+for (var y = 1; y <= 100; y++){
+    sum += y;
+}
+console.log(sum);
+
+
+
+var product = 1;
+for (var w = 1; w <= 12; w++){
+    product *= w;
+}
+console.log(product);
